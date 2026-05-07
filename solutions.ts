006e6a24c023e19d412606470e0fc95696e336ce
khalidhossain <mdkhalidhossain5000@gmail.com>
2026-05-07 10:51:56 +0600

    problem 5,6 solution is completed

--- solutions.ts
+++ solutions.ts
@@ -24,3 +24,43 @@ function checkType (input:InputType):string{
 function getProperty <T,K extends keyof T>(obj:T,key:K){
     return obj[key]
 }
+
+
+
+interface Book {
+    title:string;
+    author:string;
+    publishedYear:number;
+}
+
+function toggleReadStatus (bookDetails:Book){
+return {
+    ...bookDetails,
+    isRead:true
+}
+}
+
+
+
+class Person {
+    name:string;
+    age:number;
+
+    constructor(name:string,age:number){
+        this.name=name;
+        this.age=age
+    }
+}
+
+class Student extends Person{
+    grade:string
+    
+    constructor(name:string,age:number,grade:string){
+        super(name,age)
+        this.grade=grade
+    }
+    getDetails(){
+        return `Name: ${this.name}, Age: ${this.age}, Grade: ${this.grade}`
+    }
+}
+
