1e40127c1c4fae59f6fd8cacdb37331c63542106
khalidhossain <mdkhalidhossain5000@gmail.com>
2026-05-07 10:52:34 +0600

    all problem solutions completed

--- solutions.ts
+++ solutions.ts
@@ -1,66 +1,62 @@
-function filterEvenNumbers  (numbers:number[]):number[]{
-    const evenNumbers=numbers.filter((number)=>number%2===0)
-    return evenNumbers
+function filterEvenNumbers(numbers: number[]): number[] {
+  const evenNumbers = numbers.filter((number) => number % 2 === 0);
+  return evenNumbers;
 }
 
-
-
-function reverseString (input:string):string{
-    return input.split("").reverse().join("")
+function reverseString(input: string): string {
+  return input.split("").reverse().join("");
 }
 
+type InputType = string | number;
 
-type InputType=string | number
-
-function checkType (input:InputType):string{
-    if(typeof input==='string'){
-        return "String"
-    }
-    else{
-        return "Number"
-    }
+function checkType(input: InputType): string {
+  if (typeof input === "string") {
+    return "String";
+  } else {
+    return "Number";
+  }
 }
 
-function getProperty <T,K extends keyof T>(obj:T,key:K){
-    return obj[key]
+function getProperty<T, K extends keyof T>(obj: T, key: K) {
+  return obj[key];
 }
-
-
 
 interface Book {
-    title:string;
-    author:string;
-    publishedYear:number;
+  title: string;
+  author: string;
+  publishedYear: number;
 }
 
-function toggleReadStatus (bookDetails:Book){
-return {
+function toggleReadStatus(bookDetails: Book) {
+  return {
     ...bookDetails,
-    isRead:true
+    isRead: true,
+  };
 }
-}
-
-
 
 class Person {
-    name:string;
-    age:number;
+  name: string;
+  age: number;
 
-    constructor(name:string,age:number){
-        this.name=name;
-        this.age=age
-    }
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
 }
 
-class Student extends Person{
-    grade:string
-    
-    constructor(name:string,age:number,grade:string){
-        super(name,age)
-        this.grade=grade
-    }
-    getDetails(){
-        return `Name: ${this.name}, Age: ${this.age}, Grade: ${this.grade}`
-    }
+class Student extends Person {
+  grade: string;
+
+  constructor(name: string, age: number, grade: string) {
+    super(name, age);
+    this.grade = grade;
+  }
+  getDetails() {
+    return `Name: ${this.name}, Age: ${this.age}, Grade: ${this.grade}`;
+  }
 }
 
+function getIntersection(numbers1: number[], numbers2: number[]) {
+  const commonNumbers = numbers1.filter((number) => numbers2.includes(number));
+  return commonNumbers;
+}
